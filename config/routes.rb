2969d31f93169c3b0503e93f8c1b0ebcab956a1b
Dan Nollette <nollette.dan@gmail.com>
2017-12-01 10:09:21 -0800

    add controllers and customize devise views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root :to => "products#index"
 
-  devise_for :users
+  resources :products do
+    resources :comments
+  end
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
